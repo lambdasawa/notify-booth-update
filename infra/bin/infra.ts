037d324fa689bb230c3d7191f9b3a20bf0f6de85
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-04-12 01:52:56 +0900

    :fire: Remove infra

--- infra/bin/infra.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
-import { InfraStack } from '../lib/infra-stack';
-
-const app = new cdk.App();
-new InfraStack(app, 'NotifyBoothUpdateInfraStack');
